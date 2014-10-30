69134c85360b825f277499d0bbcb5203b82921b2
Gabor Meszaros <gabor.meszaros@logmein.com>
2014-10-30 18:00:33 +0100

    Added projects design

--- dist/features/projects/projectsCtrl.js
+++ dist/features/projects/projectsCtrl.js
@@ -3,5 +3,10 @@
  */
 angular.module("MCGTech")
     .controller("ProjectsCtrl", [ "$scope", function ($scope) {
-
+        $scope.projects = [
+            {name: "FlowDesigner", color: "#78818a"},
+            {name: "DockPanel", color: "#83c954"},
+            {name: "TankWar", color: "#ea8c6c"},
+            {name: "SpaceInvaders", color: "#0eadd4"}
+        ];
     }]);
