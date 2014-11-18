84e772aef57d06d225cccf07170f44b5207083a5
Soulskater <gmeszaros007@gmail.com>
2014-11-18 23:56:27 +0100

    Add blog service and controller

--- src/features/blog/blogCtrl.js
+++ src/features/blog/blogCtrl.js
@@ -2,7 +2,17 @@
  * Created by MCG on 2014.10.24..
  */
 angular.module("MCGTech")
-    .controller("BlogCtrl", [ "$scope", function ($scope) {
+    .controller("BlogCtrl", [ "$scope", "navigationService", "blogService", function ($scope, $navigation, $apiService) {
+        $scope.entries = [];
 
+        _init();
+        function _init() {
+            $apiService.getBlogEntries().then(function (entries) {
+                console.log(entries);
+                $scope.entries = entries;
+            });
+        }
+
+        $scope.$navigation = $navigation;
     }]);
 
---
+++ src/features/blog/blogService.js
@@ -0,0 +1,19 @@
+/**
+ * Created by MCG on 2014.10.30..
+ */
+angular.module("MCGTech")
+    .service("blogService", ["$q", "$http", function ($q, $http) {
+        var baseUrl = "http://mcgtechservice.azurewebsites.net/";
+
+        return {
+            getBlogEntries: function () {
+                var defer = $q.defer();
+                $http.get(baseUrl + "api/blog").success(function (blogs) {
+                    defer.resolve(blogs);
+                }).error(function (ex) {
+                    defer.reject(ex);
+                });
+                return defer.promise;
+            }
+        };
+    }]);
